5f28408a306442882c2f088f477a2030b67cb696
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-03-24 19:03:08 +0300

    Add profile info

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+export default class Profile extends Component {
+  state = {
+    profile: null,
+    error: ""
+  };
+
+  componentDidMount = () => {
+    this.loadUserProfile();
+  };
+
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, err) =>
+      this.setState({ profile, err })
+    );
+  }
+
+  render() {
+    const { profile } = this.state;
+    if (!profile) return null;
+    return (
+      <>
+        <h1>Profile</h1>
+        <p>{profile.nickname}</p>
+        <img
+          style={{ maxWidth: 50, maxHeight: 50 }}
+          alt="User avatar"
+          src={profile.picture}
+        />
+        <pre>{JSON.stringify(profile, null, 2)}</pre>
+      </>
+    );
+  }
 }
