0b94ff617778878ba17946e564b346c6e4a93c91
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-03-31 22:07:13 +0300

    Use context API to pass auth props to PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,34 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Route } from "react-router-dom";
+import AuthContext from "./AuthContext";
 
-function PrivateRoute({ component: Component, auth, scopes, ...rest }) {
+function PrivateRoute({ component: Component, scopes, ...rest }) {
   return (
-    <Route
-      {...rest}
-      render={props => {
-        // Redirect to login if not logged in
-        if (!auth.isAuthenticated()) return auth.login();
+    <AuthContext.Consumer>
+      {auth => (
+        <Route
+          {...rest}
+          render={props => {
+            // Redirect to login if not logged in
+            if (!auth.isAuthenticated()) return auth.login();
 
-        // Display message if user lacks requires scope(s)
-        if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
-          return (
-            <h1>
-              Unauthorized - You need the following scope(s) to view this page:{" "}
-              {scopes.join()}
-            </h1>
-          );
-        }
+            // Display message if user lacks requires scope(s)
+            if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
+              return (
+                <h1>
+                  Unauthorized - You need the following scope(s) to view this
+                  page: {scopes.join()}
+                </h1>
+              );
+            }
 
-        return <Component auth={auth} {...props} />;
-      }}
-    />
+            return <Component auth={auth} {...props} />;
+          }}
+        />
+      )}
+    </AuthContext.Consumer>
   );
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
   scopes: PropTypes.array
 };
 
